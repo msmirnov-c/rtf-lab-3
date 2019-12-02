a64eab2a987c9329e9063a658879ca6883edf6c7
dartrhevan <vovasatunkin@mail.ru>
2019-12-02 16:53:15 +0500

    frontend foundation

--- app.js
+++ app.js
@@ -9,10 +9,34 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+/*
+let expressWs = require('express-ws')(app);
+app.ws('/ws', function(ws, req, next) {
+  ws.on('message', function(msg) {
+    console.log('msg:', msg)
 
+  })
+
+});
+*/
+//const server = http.createServer(express());
+
+//initialize the WebSocket server instance
+/*const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+
+  //connection is up, let's add a simple simple event
+  ws.on('message', (message) => {
+
+    //log the received message and send it back to the client
+    console.log('received: %s', message);
+    ws.send(`Hello, you sent -> ${message}`);
+  });
+
+  //send immediatly a feedback to the incoming connection
+  ws.send('Hi there, I am a WebSocket server');
+});*/
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -32,7 +56,6 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
