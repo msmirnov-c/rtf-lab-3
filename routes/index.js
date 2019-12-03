3ce73f4a01084c3d891ce0edcfc2439e66a20a82
dartrhevan <vovasatunkin@mail.ru>
2019-12-03 21:22:19 +0500

    backend-side of authorisation - first version

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,10 @@
-//import DBProvider from "../dbprovider";
 
-const DBProvider = require("../dbprovider");
 let express = require('express');
 let router = express.Router();
 
 /* GET home page. */
 router.get('/api', function(req, res, next) {
   res.render('index', { title: 'Express' });
-});
-
-router.get('/api/user',async function(req, res, next) {
-  const db = new DBProvider();
-  await db.getUser('test_u', '1', function(err, row) {
-    res.json({login: row.login, nick: row.nick});
-  });
-
-  db.close();
-  //next();
-  //res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,32 @@
-var express = require('express');
-var router = express.Router();
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const DBProvider = require("../dbprovider");
+const express = require('express');
+const router = express.Router();
+
+router.post('/add', async function(req, res, next) {
+    const {login, nick, pass} = req.body;
+    if (!login || !nick || !pass)
+        res.send({Error: 'NO PARAMS'});
+    const db = new DBProvider();
+    await db.addUser(login, nick, pass);
+    db.close();
+    res.send('Ok');
 });
-/*
-router.get('/u', function(req, res, next) {
-  res.send('respond with a resource2');
-});*/
+
+router.post('/', function(req, res, next) {
+    const {login, nick, pass} = req.body;
+    const db = new DBProvider();
+    console.log(login, nick, pass);
+    if ((!login && !nick) || !pass)
+        res.send({Error: 'NO PARAMS'});
+    if(login)
+         db.getUserByLogin(login, pass, function(err, row) {
+            res.json({login: row.login, nick: row.nick});
+        });
+    if(nick)
+         db.getUserByLogin(nick, pass, function(err, row) {
+            res.json({login: row.login, nick: row.nick});
+         });
+    db.close();
+});
 
 module.exports = router;
