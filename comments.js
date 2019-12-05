12d3cc9b4fad12794de0130faff18de5523591ac
dartrhevan <vovasatunkin@mail.ru>
2019-12-05 21:41:05 +0500

    comments receiving added

--- comments.js
+++ comments.js
@@ -2,32 +2,23 @@ const DBProvider = require("./dbprovider");
 const {Origin, Comment} = require("./model/comment");
 
 const onconnection =  (wss, ws) => {
-    const db = new DBProvider(/*'../data.sqlite'*/);
-    //connection is up, let's add a simple simple event
+    const db = new DBProvider(/*'../data.sqlite'*/);/*
+    db.getComments(origin, function (err , rows) {
+            ws.send(JSON.stringify(rows));
+        });*/
     ws.on('message', async msg => {
         const {origin, login, pass, comment} = JSON.parse(msg);
         //log the received message and send it back to the client
         console.log('received: %s', msg);
-
         if(comment !== undefined)
             await db.addComment(new Comment(comment , login , origin));
-        //const comments = [];
         await db.getComments(origin, function (err , rows) {
-            //comments.push(row);
             wss.clients.forEach(client => {
                 console.log(err);
                 client.send(JSON.stringify(rows));
-                // client.send(`Hello, broadcast message -> ${msg}`);
             });
         });
-        /*wss.clients.forEach(client => {
-            //console.log(msg);
-            client.send(comments.toString());
-           // client.send(`Hello, broadcast message -> ${msg}`);
-        });*/
-        //ws.send(`Hi there, I am a WebSocket server ${message}`);
     });
-
     //send immediatly a feedback to the incoming connection
     ws.send('Hi there, I am a WebSocket server');
 };
--- public/javascripts/registration.js
+++ public/javascripts/registration.js
@@ -1,9 +1,36 @@
+
+const socket = new WebSocket(`ws://${document.location.host}/api/comment`);
+//console.log(document.location.host);
 document.addEventListener("DOMContentLoaded", () => {
     const usr = localStorage.getItem('user');
     console.log(usr);
     const u = JSON.parse(usr);
     if(u !== null)
-        user.innerHTML = `${u.nick}/<a onclick="localStorage.clear(); document.location.reload();">Sign out</a>`
+        user.innerHTML = `${u.nick}/<a onclick="localStorage.clear(); document.location.reload();">Sign out</a>`;
+
+    socket.onopen = function(e) {
+        console.log("[open] Соединение установлено");
+        console.log("Отправляем данные на сервер");
+        socket.send('{"origin": "0"}');
+    };
+
+    socket.onmessage = function(event) {
+        console.log(`[message] Данные получены с сервера: ${event.data}`);
+    };
+
+    socket.onclose = function(event) {
+        if (event.wasClean) {
+            console.log(`[close] Соединение закрыто чисто, код=${event.code} причина=${event.reason}`);
+        } else {
+            // например, сервер убил процесс или сеть недоступна
+            // обычно в этом случае event.code 1006
+            console.log('[close] Соединение прервано');
+        }
+    };
+
+    socket.onerror = function(error) {
+        alert(`[error] ${error.message}`);
+    };
 });
 
 async function onsent() {
