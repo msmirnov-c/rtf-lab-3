6527558c9f1b08efd19c8026ca145ffab8696fd1
Desperate Hiking <perevechenkov1999@gmail.com>
2019-12-13 21:25:44 +0500

    replase files and some front

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,9 @@
-const { authUser, postExample } = require('../workers/auth/index.js');
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/auth/:id', authUser);
-
-router.post('/post/example', postExample)
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Expressnpm'});
+});
 
 module.exports = router;
