de88e8df25e2fe1928b3d99d400190f8da88fe44
1max87 <48288135+1max87@users.noreply.github.com>
2019-12-14 22:59:18 +0500

    registration

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,23 @@
-const { authUser, postExample } = require('../workers/auth/index.js');
 var express = require('express');
 var router = express.Router();
+const CryptoJS = require('crypto-js');
+const path = require('path');
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database(path.join(__dirname, 'db.db'));
+db.run('CREATE TABLE IF NOT EXISTS users (login TEXT PRIMARY KEY, password TEXT)');
 
-/* GET users listing. */
-router.get('/auth/:id', authUser);
-
-router.post('/post/example', postExample)
+router.post('/reg', function (req, res) {
+    const {login, psw} = req.body;
+    if(!login || !psw) {
+        res.send('Error!');
+        return;
+    }
+    db.run(`INSERT INTO users VALUES('${login}','${CryptoJS.MD5(psw).toString()}')`, (err) => {
+        if(err)
+            res.send(err);
+        else
+            res.redirect('/');
+    });
+});
 
 module.exports = router;
