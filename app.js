ff6f4459cfa424d2d9919cb2d573444d04861e88
M1M0G <M1M0G@github.com>
2019-12-15 21:10:29 +0500

    COSMO APP VER 2.0
    Переделал форму регистрации добавил хеширование паролей

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const bodyParser = require('body-parser')
 var indexRouter = require('./routes/index');
-
+//const bcrypt = require( 'bcryptjs' );
 var app = express();
 
 
@@ -14,7 +14,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+//app.use(bcrypt);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- workers/register/register.js
+++ workers/register/register.js
@@ -1,27 +1,47 @@
+const bcrypt =  require('bcryptjs')
 /**
  * Метод принимающий 2 парамметра
  * @param {string} name - имя
  * @param {string} email - возраст
+ * @param {string} password - пароль
  */
 
 function register(req, res, next) {
-     const {name, email } = req.body;
-     if (!name || !email) {
+     const {name, email, password } = req.body;
+     if (!name || !email || !password) {
         return res.send({Error: 'NO PARAMS'})
      }
      //console.log(name, email);
-     const content = new Array(name, email);
+    /* 
+     Тут будем хешировать...
+     */
+     
+        // Store hash in database
+      
+    
+
+
+
+
+
+     
      const fs = require("fs");
+     const content = new Array(name, email, bcrypt.hash( password, 10, function( err, hash ){
+        return hash
+     }));
      fs.writeFile("file.txt", JSON.stringify(content), function(error){
      if(error) throw error; // если возникла ошибка
      console.log("Асинхронная запись файла завершена. Содержимое файла:");
      let data = fs.readFileSync("file.txt", "utf8");
      console.log(data);
+
+
      return res.json({Success: true});
-     //const content = new Array(name, email);
+     
     });
-    }
+}
  
+
  module.exports =  {
     register
 }
