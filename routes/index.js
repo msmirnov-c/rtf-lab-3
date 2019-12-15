6c1f188fb619aa59db0884c08aef79c3493ab74c
Yok34 <48248922+Yok34@users.noreply.github.com>
2019-12-16 02:00:46 +0500

    'changing'

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,23 @@
-const { authUser, postExample } = require('../workers/auth/index.js');
 var express = require('express');
 var router = express.Router();
+const registr = require('../workers/registration');
+const authoriz = require('../workers/authorization');
+var path = require('path');
 
-/* GET users listing. */
-router.get('/auth/:id', authUser);
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.sendFile(path.resolve('public/index.html'));
+});
 
-router.post('/post/example', postExample)
+router.get('/registration', function(req,res,next) {
+  res.sendFile(path.resolve('public/registration.html'));
+});
+
+router.get('/authorization', function(req,res,next) {
+  res.sendFile(path.resolve('public/authorization.html'));
+});
+
+router.post('/api/registration', registr)
+router.post('/api/authorization', authoriz)
 
 module.exports = router;
