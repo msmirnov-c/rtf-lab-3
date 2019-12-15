aa7ef823758fca6fa483eb3844a9014157177a50
Olga Korelina <56450175+pvpilizassal@users.noreply.github.com>
2019-12-15 23:57:42 +0500

    vol. 0.1

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookieparser');
+var logger = require('morgan');
+var indexRouter = require('./routes/index');
 
+var app = express();
+var cookieFiles = require('cookiefiles')
+
+app.use(cookieFiles({
+    name: 'files',
+    cookie: 'cookie'
+}))
+
+app.use(cookieParser());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+module.exports = app;
--- workers/auth/index.js
+++ workers/auth/index.js
@@ -1,30 +1,47 @@
+const fs = require('fs');
+const path = require('path');
+
 function authUser(req, res, next) {
-    console.log('autuser');
-    console.log(req);
-    res.json({userAuth: true})
-}
+    let user = getUser();
 
-/**
- * Метод принимающий 3 парамметра
- * @param {string} id - айди пользователя
- * @param {string} name - имя
- * @param {number} age - возраст
- */
-function postExample(req, res, next) {
-    const {id, name, age} = req.body;
-    if (!id || !name || !age) {
-        res.send({Error: 'NO PARAMS'})
+    if (user[req.body.username] === req.body.password) {
+        res.json({login: req.body.username, success: true});
+    } else {
+        res.json({error: "This login doesn't exist", success: false});
     }
-    console.log(id, age, name);
-    res.json({Success: true})
 }
 
-async function acyncFyn() {
-    await setTimeout(() => {}, 10000)
-    return true;
+function registerUser(req, res, next) {
+    let user = getUser();
+
+    if (user[req.body.username]){
+        res.json({success: false, err: 'User already exists'});
+        return
+    }
+
+    fs.appendFile(path.resolve('auth.txt'), req.body.username + ':' + req.body.password + '\n', (err) => {
+        if(err) {
+            res.json({success: false});
+            return
+        }
+
+        res.json({success: true})
+    });
 }
 
-module.exports =  {
-    authUser,
-    postExample
+function getUser() {
+    let file = fs.readFileSync(path.resolve('auth.txt'), 'utf8');
+    let line = file.split('\n');
+
+    let user = {};
+    for (let i = 0; i < line.length; i++) {
+        let user = line[i].split(':');
+        user[user[0]] = user[1];
+    }
+
+    return user;
 }
+
+module.exports = {authUser, registerUser}; 
+
+
