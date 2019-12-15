989aac7d08ac604a25991836369c38d39d0f34d5
SaekoIshikawa <52504420+SaekoIshikawa@users.noreply.github.com>
2019-12-16 02:43:29 +0500

    meow

--- workers/auth/index.js
+++ workers/auth/index.js
@@ -1,6 +1,12 @@
+const fs = require('fs');
+var path = require('path');
+
 function authUser(req, res, next) {
-    console.log('autuser');
-    console.log(req);
+    const {name} = req.body;
+    let html = fs.readFileSync('/public/index.html','utf8');
+    let nhtml = html.slice(0, html.indexOf('<aside>') + '<aside>'.length + 1);
+    nhtml = nhtml + `<h2>Привет, ${name}</h2>` + html.slice(html.indexOf('<aside>') + '<aside>'.length + 1);
+    fs.writeFileSync('/public/auth.html', nhtml);
     res.json({userAuth: true})
 }
 
@@ -11,20 +17,22 @@ function authUser(req, res, next) {
  * @param {number} age - возраст
  */
 function postExample(req, res, next) {
-    const {id, name, age} = req.body;
-    if (!id || !name || !age) {
+    const {name, password, age} = req.body;
+    if (!name || !password || !age) {
         res.send({Error: 'NO PARAMS'})
     }
-    console.log(id, age, name);
-    res.json({Success: true})
+    console.log(name, age, password);
+    const usersData = fs.readFileSync("users.txt", "utf8");
+    if (usersData.includes(`|${name},`)) {
+        res.redirect('/error');
+        res.json({Error: 'User already exists!'});
+    } else {
+        fs.appendFileSync('users.txt', `|${name}, ${password}, ${age}|`);
+        res.redirect('/auth');
+    }
 }
 
-async function acyncFyn() {
-    await setTimeout(() => {}, 10000)
-    return true;
-}
-
-module.exports =  {
+module.exports = {
     authUser,
     postExample
 }
