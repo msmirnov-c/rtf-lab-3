a7cac7943efa1487ca8617cd3ca9d30a97123360
Ulano4ka <>
2019-12-16 02:01:13 +0500

    application

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,26 @@
-const { authUser, postExample } = require('../workers/auth/index.js');
+const regirstation = require('../worker/registration/index')
+const authorization = require('../worker/authorization/index')
+
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
-/* GET users listing. */
-router.get('/auth/:id', authUser);
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  console.log(req.session.user);
+  res.cookie('userName', req.session.user.userName);
+  res.sendFile(path.resolve('views/index.html'));
+});
 
-router.post('/post/example', postExample)
+router.get('/registration', function(req, res, next) {
+  res.sendFile(path.resolve('views/registration.html'));
+});
+
+router.get('/authorization', function(req, res, next) {
+  res.sendFile(path.resolve('views/authorization.html'));
+});
+
+router.post('/api/registration', regirstation)
+router.post('/api/authorization', authorization)
 
 module.exports = router;
