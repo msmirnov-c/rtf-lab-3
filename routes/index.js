1001851548779fdc8f8e418dae68b094a96b59ed
Desperate Hiking <perevechenkov1999@gmail.com>
2019-12-16 03:09:01 +0500

    bruh

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
+const { authUser, postExample } = require('../workers/auth/index.js');
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Expressnpm'});
-});
+router.get('/auth/:id', authUser);
+
+router.post('/post/example', postExample)
 
 module.exports = router;
---
+++ workers/auth/index.js
@@ -0,0 +1,25 @@
+function authUser(req, res, next) {
+    console.log('authuser');
+    console.log(req);
+    res.json({userAuth: true})
+}
+
+/**
+ * Метод принимающий 3 парамметра
+ * @param {string} id - айди пользователя
+ * @param {string} name - имя
+ * @param {number} age - возраст
+ */
+function postExample(req, res, next) {
+    const {id, name, age} = req.body;
+    if (!id || !name || !age) {
+        res.send({Error: 'NO PARAMS'})
+    }
+    console.log(id, age, name);
+    res.json({Success: true})
+}
+
+module.exports =  {
+    authUser,
+    postExample
+};
