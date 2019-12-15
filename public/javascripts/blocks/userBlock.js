77929bf8587e60bdd7267991054ad314a615509f
FeeLeD <feeled9@gmail.com>
2019-12-16 01:54:04 +0500

    version 1.0.1

--- public/javascripts/blocks/userBlock.js
+++ public/javascripts/blocks/userBlock.js
@@ -10,24 +10,77 @@ if (now.getHours() >= 0 && now.getHours() < 6) {
     greetings = "Добрый вечер, ";
 }
 
-if (cookie != "") {
-    let login;
-    const cookies = cookie.split(';');
-    cookies.map(param => {
-        if (param.startsWith('login')) {
-            login = decodeURIComponent(param.slice(6));
-        }
+const userBlock = document.querySelector('#user-block');
+
+const userInfo = document.createElement('div');
+userInfo.id = 'user-block-info';
+
+const greetingsBlock = document.createElement('div');
+greetingsBlock.id = "greetings-block";
+
+const cookies = {};
+if (cookie !== "") {
+    cookie.split('; ').map(param => {
+        const paramParts = decodeURIComponent(param).split('=')
+        cookies[paramParts[0]] = paramParts[1];
     });
+
+    const img = document.createElement('img');
+    img.id = "arrow-img";
+    img.src = "../public/images/arrow.png";
+
+    const mainInfo = document.createElement('div');
+    mainInfo.id = "main-user-info";
+    userInfo.append(mainInfo);
+
+    const aboutInfo = document.createElement('div');
+    aboutInfo.id = "about-user-info";
+    userInfo.append(aboutInfo);
+
+    const fisrtName = document.createElement('p');
+    fisrtName.innerHTML = "<b>Имя</b>: " + cookies.firstName;
     
-    greetings += login + "!";
+    const secondName = document.createElement('p');
+    secondName.innerHTML = "<b>Фамилия</b>: " + cookies.secondName;
+    
+    const email = document.createElement('p');
+    email.innerHTML = "<b>E-mail:</b> " + cookies.email;
+    
+    const about = document.createElement('p');
+    about.innerHTML = cookies.about;
+
+    mainInfo.append(fisrtName);
+    mainInfo.append(secondName);
+    mainInfo.append(email);
+    aboutInfo.append(about);
+
+    greetingsBlock.append(img);
+}
+
+if (cookies.login !== undefined) {
+    greetings += cookies.login + "!";
 } else {
     greetings += "уважаемый Гость!";
 }
 
-const userBlock = document.querySelector('#user-block');
+const greetingsMsg = document.createElement('h1');
+greetingsMsg.id = "greetings";
+greetingsMsg.innerHTML = greetings;
 
-const message = document.createElement('h1');
-message.id = "greetings"
-message.innerHTML = greetings;
+greetingsBlock.append(greetingsMsg);
 
-userBlock.append(message);
+userBlock.append(greetingsBlock);
+
+const showInfo = () => {
+    if (cookies.login !== undefined) {
+        if (!document.querySelector('#user-block-info')) {
+            userBlock.append(userInfo);
+            document.querySelector('#arrow-img').style.animation = "rotate 0.5s";
+            document.querySelector('#arrow-img').style.transform = "scaleY(-1)"
+        } else {
+            userBlock.removeChild(userInfo);
+            document.querySelector('#arrow-img').style.animation = "rotateBack 0.5s";
+            document.querySelector('#arrow-img').style.transform = "scaleY(1)"
+        }
+    }
+}
--- workers/local/index.js
+++ workers/local/index.js
@@ -3,12 +3,20 @@ const path = require('path');
 const mainPage = (req, res, next) => {
     if (req.session.user !== undefined) {
         res.cookie('login', req.session.user.login);
+        res.cookie('firstName', req.session.user.firstName);
+        res.cookie('secondName', req.session.user.secondName);
+        res.cookie('email', req.session.user.email);
+        res.cookie('about', req.session.user.about);
     } else {
         res.clearCookie('loginexists');
         res.clearCookie('badlogin');
         res.clearCookie('bademail');
         res.clearCookie('badpass');
         res.clearCookie('login');
+        res.clearCookie('firstName');
+        res.clearCookie('secondName');
+        res.clearCookie('email');
+        res.clearCookie('about');
     }
     res.sendFile(path.resolve('views/main.html'))
 }
