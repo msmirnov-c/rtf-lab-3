43ad56f2da9e8866a6677d90c10f3ac4d8787a50
Kirill Mineev <akerlayg@gmail.com>
2019-12-15 20:01:07 +0500

    Initial

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+let indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
 
-var app = express();
+let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api', indexRouter);
+app.use('/api/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- workers/auth/index.js
+++ workers/auth/index.js
@@ -1,30 +1,43 @@
+const MongoClient = require("mongodb").MongoClient;
+const url = process.env.MONGODB_DSN;
+const mongoClient = new MongoClient(url, { useNewUrlParser: true });
+const jwt = require('jsonwebtoken');
+
+mongoClient.connect(function(err, client){});
+
 function authUser(req, res, next) {
-    console.log('autuser');
-    console.log(req);
-    res.json({userAuth: true})
+    const db = mongoClient.db("auth");
+    const collection = db.collection("users");
+
+    collection.find({username: req.body.username}).toArray(function(err, results){
+        if (results.length === 0){
+            res.json({status: "error", error: "No such user"});
+            return
+        }
+        if (results[0].password === req.body.password){
+            let token = jwt.sign({username: results[0].username}, process.env.TOKEN_KEY, {expiresIn: 24*60*60});
+            res.cookie('token', token, { maxAge: 24*60*60, httpOnly: true });
+            // res.json({status: "ok", username: req.body.username, id: results[0]._id, token: token})
+            res.redirect('/')
+        } else {
+            res.json({status: "error", error: err})
+        }
+    });
 }
 
-/**
- * Метод принимающий 3 парамметра
- * @param {string} id - айди пользователя
- * @param {string} name - имя
- * @param {number} age - возраст
- */
-function postExample(req, res, next) {
-    const {id, name, age} = req.body;
-    if (!id || !name || !age) {
-        res.send({Error: 'NO PARAMS'})
-    }
-    console.log(id, age, name);
-    res.json({Success: true})
+function registerUser(req, res, next) {
+    const db = mongoClient.db("auth");
+    const collection = db.collection("users");
+    collection.insertOne({username: req.body.username, password: req.body.password}, function(err, result){
+
+        if(err){
+            res.json({status: "error", error: err})
+        }else {
+            res.redirect('/login.html')
+            // res.json({status: "ok", username: result.ops[0].username, id: result.ops[0]._id})
+        }
+
+    });
 }
 
-async function acyncFyn() {
-    await setTimeout(() => {}, 10000)
-    return true;
-}
-
-module.exports =  {
-    authUser,
-    postExample
-}
+module.exports = {authUser, registerUser};
