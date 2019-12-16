4f14d5644d40aaee384d4f5b5b7e566a37ca5d37
Sokolov Max <48237187+Maxerdan@users.noreply.github.com>
2019-12-16 09:13:49 +0500

    Регистрация+Авторизация

--- workers/auth/index.js
+++ workers/auth/index.js
@@ -1,27 +1,48 @@
-function authUser(req, res, next) {
-    console.log('autuser');
-    console.log(req);
-    res.json({userAuth: true})
-}
+ const fs = require('fs');
+ const path = require('path');
+
 
 /**
  * Метод принимающий 3 парамметра
- * @param {string} id - айди пользователя
  * @param {string} name - имя
- * @param {number} age - возраст
+ * @param {number} pass - возраст
  */
-function postExample(req, res, next) {
-    const {id, name, age} = req.body;
-    if (!id || !name || !age) {
-        res.send({Error: 'NO PARAMS'})
+function authUser(req, res, next) {
+    const {name, pass} = req.body;
+    if (!name || !pass) {
+        res.send({Error: 'Некоторые поля незаполненны, заполните Имя/Пароль'});
     }
-    console.log(id, age, name);
-    res.json({Success: true})
+
+    let fo = fs.readFileSync(path.resolve('DB.txt'), 'utf8');
+    let name_pass = {};
+    var names = new Array();
+    var passes = new Array();
+    console.log('Количество зарегестрированных', fo.split('\n').length-1);
+    for(let x = 0; x < fo.split('\n').length-1; x++)
+    {
+        names.push(fo.split('\n')[x].split(' ')[0]);
+        passes.push(fo.split('\n')[x].split(' ')[1]);
+    }
+
+    for (let x = 0; x < fo.split('\n').length-1; x++){
+        name_pass[names[x]] = passes[x];
+    }
+    if (name_pass[name] === pass){
+        res.json({Success: true});
+    } else {
+        res.json({Error: 'Такого пользователя не существует, проверьте правильность ввода'});
+    }
+    res.json({Error: 'Ввели неверные данные, проверьте имя/пароль'});
 }
 
-async function acyncFyn() {
-    await setTimeout(() => {}, 10000)
-    return true;
+function postExample(req, res, next) {
+    const {name, pass} = req.body;
+    if (!name || !pass) {
+        res.send({Error: 'Некоторые поля остались незаполненными, заполните Имя/Пароль'});
+    }
+    fs.appendFile(path.resolve('DB.txt'), name + ' ' + pass+'\n', () => {});
+    
+    res.json({Success: true});
 }
 
 module.exports =  {
