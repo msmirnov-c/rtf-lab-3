e651869ecd8460193fd076bfc30f80b04d49d448
slavker99 <48331491+slavker99@users.noreply.github.com>
2019-12-16 16:08:46 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ const urlencodedParser = bodyParser.urlencoded({extended: false});
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
