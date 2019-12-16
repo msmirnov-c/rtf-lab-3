4a1a1f0496e6fbb61bedc5d4570aca877c6668af
Elena Fedulova <48327813+needMoreBlood@users.noreply.github.com>
2019-12-16 14:23:30 +0500

    регистрация

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,11 @@
-//const { authUser, postExample } = require('../workers/auth/index.js');
-const {register}= require('../workers/register.js');
+const { authUser, postExample } = require('../workers/register.js');
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-//router.get('/auth/:id', authUser);
-router.use(express.json());       // to support JSON-encoded bodies
-router.use(express.urlencoded()); // to support URL-encoded bodies
-//router.post('/post/example', postExample);
-router.post('/register', register);
+
+router.post('/auth', authUser);
+router.post('/post/example', postExample)
 
 module.exports = router;
 
--- workers/register.js
+++ workers/register.js
@@ -1,24 +1,50 @@
 /**
  * Метод принимающий 3 парамметра
- * @param {string} id  - айди пользователя
+ * @param {string} login  - айди пользователя
  * @param {string} name - имя
- * @param {number} age - возраст
+ * @param {number} password - возраст
  */
 
-function register(req, res, next) {
-    const {id, name, age } = req.body;
-    if (!id || !name || !age) {
+function authUser(req, res, next) {
+    const {login, password} = req.body;
+    if (!login || !password) {
+        res.send({Error: 'Некоторые поля незаполненны'});
+    }
+
+    let fo = require('fs').readFileSync(require('path').resolve('file.txt'), 'utf8');
+    let name_pass = {};
+    var names = new Array();
+    var passes = new Array();
+    console.log('Количество зарегестрированных', fo.split('\n').length-1);
+    for(let x = 0; x < fo.split('\n').length-1; x++)
+    {
+        names.push(fo.split('\n')[x].split(' ')[0]);
+        passes.push(fo.split('\n')[x].split(' ')[1]);
+    }
+
+    for (let x = 0; x < fo.split('\n').length-1; x++){
+        name_pass[names[x]] = passes[x];
+    }
+    if (name_pass[login] === pass){
+        res.json({Success: true});
+    } else {
+        res.json({Error: 'Такого пользователя не существует, проверьте правильность ввода'});
+    }
+    res.json({Error: 'Ввели неверные данные, проверьте имя/пароль'});
+}
+
+function postExample(req, res, next) {
+    const {login, name, password} = req.body;
+    if (!login || !name || !password) {
         res.send({Error: 'NO PARAMS'})
     }
-    console.log(id, age, name);
-    res.json({Success: true});
-    const content = (id, name, age);
+    console.log(login, name, password);
+    res.json({Success: true})
     file_put_contents('file.txt', req.body);
-    $f = fopen('/file.txt', 'a+' );
-    fwrite($f, "Имя пользователя: ", req.body," \n");
-    
-   }
+
+}
 
 module.exports =  {
-   register
+    authUser,
+    postExample
 }
