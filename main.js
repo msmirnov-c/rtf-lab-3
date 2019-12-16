2b3fba549c1a8281fa20af90239cdea3b3572825
yakov-golencev <yakov.golencev@gmail.com>
2019-12-16 09:23:13 +0500

    website
    без адаптивной верстки, но через grid
    авторизация и регистрация работают, но после авторизации\регистрации домашняя страничка никак не изменяется
    Тема:  Как был придуман JavaScript, история создания, текущее развитие
    Ссылка на сайт будет далее

---
+++ main.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const router = require('./routes/index')
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', router);
+
+app.listen(3000);
