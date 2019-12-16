d6f02069276e7bd4562e0e21a5ac0a0b7b92d944
Daniil <p.danya@mail.ru>
2019-12-16 21:15:50 +0500

    ver1.2

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,4 +1,12 @@
 console.log(document.cookie);
+
+let h = (new Date()).getHours();                  
+if (h > 3 && h <  12) document.getElementById("hour").innerHTML = "Доброе утро, ";                    
+if (h > 11 && h <  19) document.getElementById("hour").innerHTML ="Добрый день,";                   
+if (h > 18 && h <  24) document.getElementById("hour").innerHTML = "Добрый вечер,";                 
+if (h > 23 || h <  4 ) document.getElementById("hour").innerHTML = "Привет полуночникам,";
+
+
 let cookies = {};
 
 document.cookie.split("; ").map(parameter => {
@@ -9,13 +17,13 @@ document.cookie.split("; ").map(parameter => {
   const greetings = document.createElement('h1');
 
   if (cookies.email === undefined) {
-    greetings.innerHTML = "Hello, guest";
+    document.getElementById("greeting").innerHTML = "Гость";
     document.getElementById('auth').hidden = false;
     document.getElementById('reg').hidden = false;
     document.getElementById('logout').hidden = true;
 
   } else {
-    greetings.innerHTML = "Hello, " + cookies.name + ' ' + cookies.surname;
+    document.getElementById("greeting").innerHTML = "" + cookies.name + ' ' + cookies.surname;
     document.getElementById('auth').hidden = true;
     document.getElementById('reg').hidden = true;
     document.getElementById('logout').hidden = false;
