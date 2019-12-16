94dc1b0098c2071a8cd98c8e66ddcf10faa648a1
Sokolov Max <48237187+Maxerdan@users.noreply.github.com>
2019-12-16 09:01:47 +0500

    Удалил старый проект(сломал через npm), Фронт-Бэк, Регистрация + Авторизация

--- lab3-web/routes/index.js
+++
@@ -1,8 +0,0 @@
-const { authUser, postExample } = require('../workers/auth/index.js');
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/auth/:id', authUser);
-router.post('/post/example', postExample)
-module.exports = router;
