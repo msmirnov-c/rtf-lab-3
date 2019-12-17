64fdca1d39ff447889e94583d531e024a0c0983a
OvchinnikovJMA <DegraDanko@gmail.com>
2019-12-17 23:24:37 +0500

    Добавлена история
    Авторизация рабочая.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-const { registrate } = require('../workers/auth/index.js');
+const { registrate, authenticate } = require('../workers/auth/index.js');
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-
-router.post('/post/example', registrate)
+router.post('/post/auth', authenticate);
+router.post('/post/example', registrate);
 
 module.exports = router;
--- workers/auth/index.js
+++ workers/auth/index.js
@@ -1,5 +1,5 @@
 /**
- * Метод принимающий 3 парамметра
+ * Регистрация принимающий 3 парамметра
  * @param {string} email - email
  * @param {string} logname - имя
  * @param {string} password - пароль
@@ -12,22 +12,50 @@ function registrate(req, res, next) {
     const fs = require("fs");
     let data = {
         email: email,
-        Login: logname,
+        logname: logname,
         password: password
-    }
+    };
     fs.appendFile("data.txt", JSON.stringify(data)+'\n', function(error){
         if(error) throw error;
         console.log("Запись файла завершена. Содержимое файла:");
         let inf = fs.readFileSync("data.txt", "utf8");
-
         console.log(inf);
-
-        res.json(alert('Регистрация прошла успешно!'));
     })
-    
+    res.send({Success: true});
 }
 
 
+/**
+ * Вход принимающий 3 парамметра
+ * @param {string} email - email
+ * @param {string} logname - имя
+ * @param {string} password - пароль
+ */
+function authenticate(req, res, next){
+    const {email, logname, password} = req.body;
+    if (!email || !logname || !password) {
+        res.send({Error: 'NO Enter'})
+    }
+    const fs = require("fs");
+    fs.readFile("data.txt", "utf8", function(error, data){
+        if(error) throw error;
+        let mas = data.split("\n");
+        let mas = data.split("\n");
+        mas.forEach(element => {
+            let inf = JSON.parse(element);
+            console.log(inf);
+            console.log(email, logname, password);
+            if(logname == inf.logname)
+            {
+                console.log("Очень рад вас видеть, " + inf.logname);
+                return res.send({Success: true});
+            }
+        });
+        return res.send({Success: false});
+    }) 
+}
+
 module.exports =  {
-    registrate
+    registrate,
+    authenticate
 };
