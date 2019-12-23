3e224f5df71a34d989f020d6fe66ae60eb5735bf
Marina <marina-maksa@mail.ru>
2019-12-23 19:23:04 +0500

    -

--- workers/auth/index.js
+++ workers/auth/index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 /**
  * Метод принимающий 2 парамметра
  * @param {string} login - логин
- * @param {number} password - пароль
+ * @param {string} password - пароль
  */
 function auth(req, res, next) {
     const {login, password} = req.body;
@@ -14,7 +14,7 @@ function auth(req, res, next) {
     if (usersData.includes(`"login":"${login}","password":"${password}"`)){
         res.redirect('/public/index.html');
     } else {
-        res.req({Error: 'Такого пользователя нет в базе. Может введены неверные данные'});
+        res.json({Error: 'Такого пользователя нет в базе. Может введены неверные данные'});
     }
 }
 
@@ -22,19 +22,19 @@ function auth(req, res, next) {
  * Метод, принимающий 3 парамметра
  * @param {string} login - логин
  * @param {string} email - почта
- * @param {number} password - пароль
+ * @param {string} password - пароль
  */
 function register(req, res, next) {
     const {login, email, password} = req.body;
     console.log(login, email, password);
 
     const usersData = fs.readFileSync("users.txt", "utf8");
-    if (usersData.includes(`"login":"${login}"`)) {
-        res.req({Error: 'Такой логин уже существует. Придумайте новый'});
-    } else {
+    if (!usersData.includes(`"login":"${login}"`)) {
         fs.appendFileSync('users.txt', ` "login":"${login}","password":"${password}","email":"${email}"` + '\n');
         res.redirect('/public/index.html');
-    }    
+    } else {
+        res.json('Такой логин уже существует.');
+    }
 }
 
 module.exports = {
