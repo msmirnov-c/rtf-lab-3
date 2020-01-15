3c6c1225ef8950e40e18aa3e214f0a3d581ad0b9
AndrewMarshal <37539223+AndrewMarshal@users.noreply.github.com>
2020-01-15 19:11:37 +0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/auth/:id', authUser);
-router.post('/post/example', postExample)
+//router.get('/auth/:id', authUser);
+router.use(express.json());
+router.use(express.urlencoded());
+router.post('/auth', auth)
 module.exports = router;
